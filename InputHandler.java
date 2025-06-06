c0d42b4c3680f60fc2c6902bac4b502c425a946d
Benchea Diana <162334377+BencheaDiana@users.noreply.github.com>
2025-06-06 03:12:24 +0300

    Delete InputHandler.java

--- InputHandler.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class InputHandler {
-    public static String askForInputClassName() {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print("Enter class name (e.g., SampleTestClass): ");
-        return scanner.nextLine().trim();
-    }
-} 
