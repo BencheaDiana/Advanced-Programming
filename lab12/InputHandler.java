79507a509f9dd517423d42022c76fae3f662f8c3
Benchea Diana <162334377+BencheaDiana@users.noreply.github.com>
2025-06-06 10:16:16 +0300

    Update InputHandler.java

--- lab12/InputHandler.java
+++ lab12/InputHandler.java
@@ -3,7 +3,7 @@ package org.example;
 public class InputHandler {
     public static String askForInputClassName() {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print("Enter class name (e.g., SampleTestClass): ");
+        System.out.print("Input your class's name: ");
         return scanner.nextLine().trim();
     }
 } 
